cc07d7d640b1e608e482e6d4f5cd7a03273c27ca
benjaminmattar <116775883+benjaminmattar@users.noreply.github.com>
2024-07-01 01:43:45 +0000

    assainatura 5 cancelada

--- demo/src/main/resources/db/data.sql
+++ demo/src/main/resources/db/data.sql
@@ -6,4 +6,4 @@ INSERT INTO assinatura (codigo, cliente_codigo, aplicativo_codigo, inicio_vigenc
 INSERT INTO assinatura (codigo, cliente_codigo, aplicativo_codigo, inicio_vigencia, fim_vigencia) VALUES (2, 2, 2, '2024-02-01', '2024-12-31');
 INSERT INTO assinatura (codigo, cliente_codigo, aplicativo_codigo, inicio_vigencia, fim_vigencia) VALUES (3, 3, 3, '2024-03-01', '2024-12-31');
 INSERT INTO assinatura (codigo, cliente_codigo, aplicativo_codigo, inicio_vigencia, fim_vigencia) VALUES (4, 4, 4, '2024-04-01', '2024-12-31');
-INSERT INTO assinatura (codigo, cliente_codigo, aplicativo_codigo, inicio_vigencia, fim_vigencia) VALUES (5, 5, 5, '2024-05-01', '2024-12-31');
+INSERT INTO assinatura (codigo, cliente_codigo, aplicativo_codigo, inicio_vigencia, fim_vigencia) VALUES (5, 5, 5, '2024-05-01', '2024-06-01');
